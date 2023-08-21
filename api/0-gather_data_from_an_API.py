3145525b19be9762747e360d5745ec5d81374847
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-21 15:36:48 +0200

    Correction print

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,8 +5,9 @@ from sys import argv
 
 API_URL = 'https://jsonplaceholder.typicode.com'
 
+
 if __name__ == '__main__':
-    # Récupérer l'ID de l'employé depuis les arguments de ligne de commande
+    # Récupérer l'ID de l'employé depuis les args de ligne de commande
     employee_id = argv[1]
 
     # Obtenir les informations sur l'utilisateur depuis l'API
@@ -16,14 +17,16 @@ if __name__ == '__main__':
     todo_data = requests.get(f"{API_URL}/todos?userId={employee_id}").json()
 
     # Filtrer les tâches terminées
-    completed_tasks = [task['title'] for task in todo_data if task['completed']]
+    completed_task_titles = [task['title'] for task in todo_data if task['completed']]
 
     # Nom de l'employé, nombre de tâches terminées et nombre total de tâches
     employee_name = user_data["name"]
-    num_completed_tasks = len(completed_tasks)
+    num_completed_tasks = len(completed_task_titles)
     total_tasks = len(todo_data)
 
     # Afficher les informations
-    print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
-    for task in completed_tasks:
-        print(f"\t{task}")
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, num_completed_tasks, total_tasks))
+
+    for task_title in completed_task_titles:
+        print(f"\t {task_title}")
