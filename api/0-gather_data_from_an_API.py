3f2b2bc095bce852ca8227a81f25f2ab3ff54a03
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-21 15:14:29 +0200

    script that, using this REST API, for a given employee ID

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
-"""Script that, using this REST API, for a given employee"""
+"""script that, using this REST API, for a given employee ID"""
 import requests
 from sys import argv
 
 
+if __name__ == '__main__':
+    URL = 'https://jsonplaceholder.typicode.com'
 
+    # Récupérer l'ID de l'employé depuis les arguments de ligne de commande
+    employee_id = argv[1]
+
+    # Obtenir les infos sur l'user
+    user_data = requests.get(f"{API_URL}/users/{employee_id}").json()
+
+    # Obtenir la liste des tâches à faire pour l'employé
+    todo_data = requests.get(f"{API_URL}/todos?userId={employee_id}").json()
+
+    # Filtrer les tâches terminées
+    completed_task_titles = [task['title'] for task in todo_data if task['completed']]
+
+    # Calculer le nombre total de tâches
+    total_tasks = len(todo_data)
+
+    # Afficher le résultat
+    print(f"Employee {user_data['name']} is done with tasks({len(completed_tasks)}/{total_tasks}):")
+    for task in sum_tasks:
+        print(f"    {task}")
 
