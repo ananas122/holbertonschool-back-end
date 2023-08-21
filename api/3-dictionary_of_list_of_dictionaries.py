63d63a8221bb1e30cc04cd6ba89bc25931fc5be7
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-21 16:02:33 +0200

    export data in json

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""Script to export data in the JSON format."""
+import json
+import requests
+
+API_URL = 'https://jsonplaceholder.typicode.com'
+
+if __name__ == '__main__':
+    # Récupérer les données de tous les utilisateurs depuis l'API
+    user_data = requests.get(f"{API_URL}/users").json()
+
+    # Créer un dictionnaire pour stocker les données de tous les employés
+    all_employee_data = {}
+
+    # Parcourir chaque utilisateur pour obtenir ses tâches et les stocker dans le dictionnaire
+    for user in user_data:
+        user_id = str(user['id'])
+        tasks_data = requests.get(f"{API_URL}/todos?userId={user_id}").json()
+
+        # Préparer les données des tâches pour l'utilisateur actuel
+        tasks_info = [
+            {"username": user['username'], "task": task['title'], "completed": task['completed']}
+            for task in tasks_data
+        ]
+
+        all_employee_data[user_id] = tasks_info
+
+    # Écrire les données dans un fichier JSON
+    with open("todo_all_employees.json", mode='w') as json_file:
+        json.dump(all_employee_data, json_file)
+
+    print("Les données ont été exportées vers todo_all_employees.json")
